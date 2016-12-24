6d5685caa01bdca84a327c953449ebc9de5aba60
lberger1993 <berger.luciaeve@gmail.com>
2016-12-24 16:43:18 -0500

    Adds google maps to pip dependencies

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template, json, request
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+from flask_googlemaps import GoogleMaps
+
 app = Flask(__name__)
+
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Happy24Fish'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+GoogleMaps(app, key="AIzaSyAEM0Xwkw_xhZFEmpo2mVKSM7h3fKdCRFM")
+
 
 @app.route("/")
 def hello():
@@ -9,19 +24,46 @@ def hello():
 def showSignUp():
     return render_template('signup.html')
 
-@app.route('/signUp',methods=['POST'])
+@app.route('/showChart')
+def showChart():
+    return render_template('chart.html')
+
+@app.route('/signUp',methods=['POST','GET'])
 def signUp():
- 
-    # read the posted values from the UI
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
- 
-    # validate the received values
-    if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        # validate the received values
+        if _name and _email and _password:
+            
+            # All Good, let's call MySQL
+            
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            print(_hashed_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password[0:6]))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close() 
+        conn.close()
+
+@app.route('/getChartData',methods=['GET'])
+def getChartData():
+    print("I get here")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
